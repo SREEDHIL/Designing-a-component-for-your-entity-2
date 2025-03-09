2d3d4b5a486c9f307d2af712b45f142b13cc39f9
SREEDHIL <sreedhilpavishanker@gmail.com>
2025-03-09 21:53:26 +0530

    updated

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,13 @@
-// write button card here
+import React from 'react'
+
+const ViewProductButton = () => {
+  return (
+    <div>
+      <button onClick={()=> alert("Product Viewed !")}>
+        View Product
+      </button>
+    </div>
+  )
+}
+
+export default ViewProductButton
